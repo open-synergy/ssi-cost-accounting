a1426d9a1764829b5582988da21204189dd081d2
Andhitia Rama <andhitia.r@gmail.com>
2022-08-15 17:44:28 +0700

    [ssi_cost_accounting] 14.0.1.0.0

---
+++ ssi_cost_accounting/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Cost Accounting",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_financial_accounting",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+        "views/account_analytic_account_views.xml",
+        "views/account_analytic_group_views.xml",
+        "views/account_analytic_tag_views.xml",
+    ],
+    "demo": [],
+}
