3297c27ffc6ccc9bd5cb5c4d12a592b5c754728e
Andhitia Rama <andhitia.r@gmail.com>
2023-06-17 21:48:49 +0700

    [ssi_cost_accounting] 14.0.1.2.0

---
+++ ssi_cost_accounting/models/account_analytic_account.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class AccountAnalyticAccount(models.Model):
+    _name = "account.analytic.account"
+    _inherit = [
+        "account.analytic.account",
+        "mixin.date_duration",
+    ]
+
+    # Mixin duration attribute
+    _date_start_readonly = False
+    _date_end_readonly = False
+    _date_start_required = False
+    _date_end_required = False
